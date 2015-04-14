589ec337429ff444409cb24070a66f9810ca3dc2
Sean Smith <seansmith911@gmail.com>
2015-04-14 15:50:26 -0400

    established relationship between list and words

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413210138) do
+ActiveRecord::Schema.define(version: 20150414194345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,10 @@ ActiveRecord::Schema.define(version: 20150413210138) do
     t.text     "definition"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "list_id"
   end
 
+  add_index "words", ["list_id"], name: "index_words_on_list_id", using: :btree
+
+  add_foreign_key "words", "lists"
 end
--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -11,9 +11,9 @@ FactoryGirl.define do
     sequence(:name) {|n| "List_#{n}" }
   end
 
-  factory :word do
-    sequence(:name) {|n| "Word_#{n}" }
-    sequence(:definition) {|n| "This is the definition for Word_#{n}" }
-  end
+  # factory :word do
+  #   sequence(:name) {|n| "Word_#{n}" }
+  #   sequence(:definition) {|n| "This is the definition for Word_#{n}" }
+  # end
 
 end
