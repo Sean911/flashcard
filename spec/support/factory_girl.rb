f49aae990d763e0fd777a3d7dcf6ec4773c0252b
Sean Smith <seansmith911@gmail.com>
2015-04-14 15:40:23 -0400

    added factory for words

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -8,7 +8,12 @@ FactoryGirl.define do
   end
 
   factory :list do
-    # name "List_1"
     sequence(:name) {|n| "List_#{n}" }
   end
+
+  factory :word do
+    sequence(:name) {|n| "Word_#{n}" }
+    sequence(:definition) {|n| "This is the definition for Word_#{n}" }
+  end
+
 end
