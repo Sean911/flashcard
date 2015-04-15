a0cd0a2dc2cb49e92ab62f299f9eb4ef701a4fb6
Omid Bachari <omid.bachari@launchacademy.co>
2015-04-15 15:25:57 -0400

    WIP writing tests

--- spec/features/lists_spec.rb
+++ spec/features/lists_spec.rb
@@ -5,23 +5,28 @@ feature 'user sees lists of words', %Q{
   I want to see what lists are available
   So that I can choose one for my quiz
 } do
-  let!(:user) { FactoryGirl.create(:user) }
-  let!(:list) { FactoryGirl.create(:list) }
 
+  let!(:list) { FactoryGirl.create(:list) }
+  let!(:user) { FactoryGirl.create(:user) }
   before :each do
     sign_in user
   end
 
   scenario 'see all available lists' do
     visit lists_path
-
     expect(page).to have_content('List_')
   end
 
   scenario "user creates new list" do
    visit lists_path
    click_on "Create a new list"
-
    expect(page).to have_content("Name")
   end
+
+  scenario "see universal lists"
+  scenario "clicking on a list"
+  scenario "edit list"
+  scenario "deleting a list"
+  scenario "user cannot edit someone elses list"
+  scenario "user cannot delete someone elses list"
 end
