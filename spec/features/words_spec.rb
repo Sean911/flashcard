90075123a731396efda795f11112d87075ea1770
Sean Smith <seansmith911@gmail.com>
2015-04-16 15:28:26 -0400

    added tests

--- spec/features/words_spec.rb
+++ spec/features/words_spec.rb
@@ -8,6 +8,7 @@ feature 'user sees a list of words', %Q{
 } do
   let!(:user) { FactoryGirl.create(:user) }
   let!(:word) { FactoryGirl.create(:word) }
+  let!(:list) { FactoryGirl.create(:list, owner_id = user.id) }
 
   before :each do
     sign_in user
@@ -19,4 +20,9 @@ feature 'user sees a list of words', %Q{
 
     expect(page).to have_content(word.name)
   end
+
+  scenario 'add words to list'
+    visit list_path(list)
+  scenario 'remove words from list'
+
 end
