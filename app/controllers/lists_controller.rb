c4ce21a53a39a634f1711cacce36402b04dfaef5
Sean Smith <seansmith911@gmail.com>
2015-04-16 15:19:09 -0400

    list testing works except delete

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,12 @@
 class ListsController < ApplicationController
+
   def index
     @lists = List.all
+    # if !current_user.nil?
+    #   @lists = List.where(user_id: [nil, "", current_user.id])
+    # else
+    #   @lists = List.where(user_id: [nil, ""])
+    # end
   end
 
   def show
@@ -10,5 +16,36 @@ class ListsController < ApplicationController
 
   def new
     @list = List.new
+  end
+
+  def create
+    @list = current_user.lists.new(list_params)
+
+    if @list.save
+      flash[:notice] = "List created!"
+      redirect_to @list
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    if current_user.id == list.user_id
+      @list.destroy
+      flash[:notice] = "List deleted!"
+      redirect_to lists_path
+    else
+      flash[:notice] = "You can't delete someone else's list!"
+      render :show
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(
+      :name, :user_id
+      )
   end
 end
--- spec/features/lists_spec.rb
+++ spec/features/lists_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'pry'
 
 feature 'user sees lists of words', %Q{
   As a user
@@ -6,27 +7,62 @@ feature 'user sees lists of words', %Q{
   So that I can choose one for my quiz
 } do
 
-  let!(:list) { FactoryGirl.create(:list) }
   let!(:user) { FactoryGirl.create(:user) }
-  before :each do
-    sign_in user
-  end
+  let!(:user2) { FactoryGirl.create(:user) }
+  let!(:user3) { FactoryGirl.create(:user) }
+  let!(:list) { FactoryGirl.create(:list) }
+  let!(:list2) { FactoryGirl.create(:list) }
+  let!(:owned_list) { FactoryGirl.create(:list, user_id: user3.id) }
+
+  #
+  # before :each do
+  #   sign_in user
+  # end
 
   scenario 'see all available lists' do
+    sign_in user
     visit lists_path
-    expect(page).to have_content('List_')
+    expect(page).to have_content(list.name)
   end
 
-  scenario "user creates new list" do
-   visit lists_path
-   click_on "Create a new list"
-   expect(page).to have_content("Name")
+  scenario "user successfully creates new list" do
+    sign_in user
+    visit lists_path
+    click_on "Create a new list"
+    fill_in("Name", with: list.name)
+    click_on("Create List")
+
+    expect(page).to have_content("List created!")
   end
 
-  scenario "see universal lists"
-  scenario "clicking on a list"
-  scenario "edit list"
-  scenario "deleting a list"
-  scenario "user cannot edit someone elses list"
-  scenario "user cannot delete someone elses list"
+  scenario "user fails to provide valid info to create new list" do
+    sign_in user
+    visit lists_path
+    click_on "Create a new list"
+    click_on("Create List")
+
+    expect(page).to have_content("Name can't be blank")
+  end
+
+   scenario "see universal lists" do
+     sign_in user
+     visit lists_path
+     expect(page).to_not have_content(owned_list.name)
+   end
+
+  scenario "deleting a list" do
+    sign_in owned_list.user
+    visit list_path(owned_list)
+    click_button("Delete list")
+
+    expect(page).to have_content("List deleted!")
+  end
+
+  scenario "user cannot delete someone elses list" do
+    sign_in user
+    visit list_path(owned_list)
+    click_button("Delete list")
+
+    expect(page).to_not have_button("Delete list")
+  end
 end
