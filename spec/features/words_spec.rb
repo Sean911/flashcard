f5a392afad6350c08d458a0bc2699408aded6c90
Sean Smith <seansmith911@gmail.com>
2015-04-17 10:59:23 -0400

    heroku deployment to gemfile, words testing

--- spec/features/words_spec.rb
+++ spec/features/words_spec.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require 'rails_helper'
 require 'pry'
 
@@ -9,45 +8,29 @@ feature 'user sees a list of words', %Q{
 } do
   let!(:user) { FactoryGirl.create(:user) }
   let!(:word) { FactoryGirl.create(:word) }
-  let!(:list) { FactoryGirl.create(:list, owner_id = user.id) }
+  let!(:list) { FactoryGirl.create(:list, owner_id: user.id) }
 
   before :each do
     sign_in user
   end
 
   scenario 'see all words in a list' do
-    visit lists_path
-    click_on word.list.name
+    visit list_path(list)
 
     expect(page).to have_content(word.name)
   end
 
-  scenario 'add words to list'
+  scenario 'add words to list' do
     visit list_path(list)
+    click_button("Add a word")
+    fill_in("name", with: "sailboat")
+    fill_in("description", with: "a boat propelled by sails")
+    click_button("Create")
+
+    expect(page).to have_content("Word successfully added!")
+  end
+
   scenario 'remove words from list'
 
+
 end
-=======
-# require 'rails_helper'
-# require 'pry'
-#
-# feature 'user sees a list of words', %Q{
-#   As a user
-#   I want to see what lists are available
-#   So that I can choose one for my quiz
-# } do
-#   let!(:user) { FactoryGirl.create(:user) }
-#   let!(:word) { FactoryGirl.create(:word) }
-#
-#   before :each do
-#     sign_in user
-#   end
-#
-#   scenario 'see all words in a list' do
-#     visit lists_path
-#     click_on word.list.name
-#
-#     expect(page).to have_content(word.name)
-#   end
-# end
->>>>>>> master
